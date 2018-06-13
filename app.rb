11da928205c46428ecd14998f179ffb73ba26209
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 14:35:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    
+
   end
 
   get '/hometown' do
     "My name is Temitope Akintoye"
   end
+
+  get '/favorite-song' do
+    "My name is Temitope Akintoye"
+  end
+  
 end
